44f1908b170d330a9afe67d7b32a7abfe8e14162
Sahil2720kumar <92155798+Sahil2720kumar@users.noreply.github.com>
2023-08-09 19:00:08 +0530

    Add files via upload

---
+++ src/data/servicesArr.js
@@ -0,0 +1,40 @@
+const servicesArr=[
+	{
+		id:1,
+		iconClass:"bi bi-briefcase",
+		name:"WEB DESIGN",
+		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+	},
+	{
+		id:2,
+		iconClass:"bi bi-card-checklist",
+		name:"WEB DEVELOPMENT",
+		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+	},
+	{
+		id:3,
+		iconClass:"bi bi-bar-chart",
+		name:"PHOTOGRAPHY",
+		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+	},
+	{
+		id:4,
+		iconClass:"bi bi-binoculars",
+		name:"RESPONSIVE DESIGN",
+		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+	},
+	{
+		id:5,
+		iconClass:"bi bi-brightness-high",
+		name:"GRAPHIC DESIGN",
+		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+	},
+	{
+		id:6,
+		iconClass:"bi bi-calendar4-week",
+		name:"MARKETING SERVICES",
+		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident.",
+	},
+]
+
+export {servicesArr}
